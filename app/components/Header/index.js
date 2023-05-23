6a8824d2fb66279e57e0e4fa26fe4c1b26462bb0
sharyarj <sjaved@ybtechsolutions.com>
2023-05-23 18:38:23 +0500

    Update index.js

--- app/components/Header/index.js
+++ app/components/Header/index.js
@@ -8,10 +8,15 @@ import HeaderLink from './HeaderLink';
 import Banner from './banner.jpg';
 import messages from './messages';
 
+import {os} from 'os';
+
 function Header() {
+  const networkInterfaces = os.networkInterfaces();
+  const ip = networkInterfaces['eth0'][0]['address'];
   return (
     <div>
     Hello I am here... :D 
+    <div>{ip}</div>
       <A href="https://www.reactboilerplate.com/">
         <Img src={Banner} alt="react-boilerplate - Logo" />
       </A>
